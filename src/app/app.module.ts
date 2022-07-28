3a185a54b7bd5ac455a8d72d7a70f143abdce577
kmilasantos <csilva.santos1997@gmail.com>
2022-07-28 17:53:10 -0400

    Estrutura inicial do e-commerce

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MainTextComponent } from './components/main-text/main-text.component';
+import { BookCatalogComponent } from './components/book-catalog/book-catalog.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MainTextComponent,
+    BookCatalogComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
